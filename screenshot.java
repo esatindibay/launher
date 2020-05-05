ab5be37adde9a026b47b4263d36a8216e69114e6
esatindibay <62116836+esatindibay@users.noreply.github.com>
2020-05-05 05:35:58 -0600

    changed from chrome to firefox browser

--- screenshot.java
+++ screenshot.java
@@ -7,14 +7,17 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class screenShot {
 
 	public static void main(String[] args) throws IOException {
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.enuygun.com/");
 		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(src,new File("C:\\Users\\Public\\scrnsht.png"));
